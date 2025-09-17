156302c287be76ea9f14e0ba4b8a293d9b6b8166
hjc2000 <hjc.666@foxmail.com>
2025-09-17 11:06:48 +0800

    feat(clock): 优化 Pclk1ClockSignal 类并添加 InputDivider 方法
    - 移除未使用的 Frequency 方法
    - 添加 InputDivider 方法，用于获取 PCLK1 的输入分频系数
    - 在 Configure 方法中添加 _configured 标志，确保配置成功后设置为已配置状态
    - 更新类的成员变量访问权限，提高封装性

--- private_src/clock/Pclk1ClockSignal.h
+++ private_src/clock/Pclk1ClockSignal.h
@@ -10,18 +10,16 @@ namespace bsp
 	class Pclk1ClockSignal :
 		public base::clock::clock_source_handle
 	{
-	public:
-		/* #region Frequency */
+	private:
+		uint32_t _input_divider = 4;
+		inline static bool _configured = false;
 
+	public:
 		virtual base::unit::MHz Frequency() const override
 		{
 			uint32_t value = HAL_RCC_GetPCLK1Freq();
 			return base::unit::MHz{base::unit::Hz{value}};
 		}
-
-		/* #endregion */
-
-		/* #region Configure */
 
 		virtual void Configure(std::map<std::string, uint32_t> const &channel_factor_map) override
 		{
@@ -39,26 +37,31 @@ namespace bsp
 			case 1:
 				{
 					def.APB1CLKDivider = RCC_APB1_DIV1;
+					_input_divider = 1;
 					break;
 				}
 			case 2:
 				{
 					def.APB1CLKDivider = RCC_APB1_DIV2;
+					_input_divider = 2;
 					break;
 				}
 			case 4:
 				{
 					def.APB1CLKDivider = RCC_APB1_DIV4;
+					_input_divider = 4;
 					break;
 				}
 			case 8:
 				{
 					def.APB1CLKDivider = RCC_APB1_DIV8;
+					_input_divider = 8;
 					break;
 				}
 			case 16:
 				{
 					def.APB1CLKDivider = RCC_APB1_DIV16;
+					_input_divider = 16;
 					break;
 				}
 			default:
@@ -74,8 +77,29 @@ namespace bsp
 			{
 				throw std::runtime_error{"时钟信号配置失败"};
 			}
+
+			// 配置成功，设置为已配置状态。
+			_configured = true;
 		}
 
-		/* #endregion */
+		///
+		/// @brief 获取 PCLK1 的输入分频系数。
+		///
+		/// @note 这个有什么用呢？为什么要获取分频系数？
+		/// 因为在 APB1 总线上的定时器的输入端有个倍频器，只要 PCLK1 的输入分频器的分频系数不是 1,
+		/// 这个倍频器就会把输入定时器模块的 PCLK1 倍频。
+		///
+		/// @return
+		///
+		uint32_t InputDivider() const
+		{
+			if (!_configured)
+			{
+				throw std::runtime_error{CODE_POS_STR + "没有用本类配置过 PCLK1, 无法使用本属性。"};
+			}
+
+			return _input_divider;
+		}
 	};
+
 } // namespace bsp
