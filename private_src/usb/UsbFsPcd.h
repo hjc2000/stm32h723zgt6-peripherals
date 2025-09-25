dc6dbc8ee8f36d2ca66c88f39e8f86f4061fef6d
hjc2000 <hjc.666@foxmail.com>
2025-09-25 14:45:09 +0800

    feat(usb): 添加USB回调函数支持
    在UsbFsPcd.h中引入std::function以支持设置各类USB事件回调函数，
    包括SOF、Setup Stage、Reset、Suspend、Resume、Connect和Disconnect事件。
    同时添加了对应的SetCallback函数，用于注册这些回调，并在中断处理中调用。
    为保证线程安全，在设置回调时临时关闭并重新开启中断。

--- private_src/usb/UsbFsPcd.h
+++ private_src/usb/UsbFsPcd.h
@@ -1,9 +1,11 @@
 #pragma once
+#include "base/embedded/interrupt/interrupt.h"
 #include "base/embedded/usb/usb_fs_pcd_handle.h"
 #include "base/UsageStateManager.h"
 #include "hal.h" // IWYU pragma: keep
 #include "usb_fs_pcd_handle.h"
 #include <cstdint>
+#include <functional>
 
 namespace bsp
 {
@@ -28,34 +30,70 @@ namespace bsp
 
 		void InitializeCallback();
 
+		std::function<void()> _sof_callback;
+		std::function<void()> _setup_stage_callback;
+		std::function<void()> _reset_callback;
+		std::function<void()> _suspend_callback;
+		std::function<void()> _resume_callback;
+		std::function<void()> _connect_callback;
+		std::function<void()> _disconnect_callback;
+
 		/* #region USB 回调 */
 
 		void OnSofCallback()
 		{
+			if (_sof_callback)
+			{
+				_sof_callback();
+			}
 		}
 
 		void OnSetupStageCallback()
 		{
+			if (_setup_stage_callback)
+			{
+				_setup_stage_callback();
+			}
 		}
 
 		void OnResetCallback()
 		{
+			if (_reset_callback)
+			{
+				_reset_callback();
+			}
 		}
 
 		void OnSuspendCallback()
 		{
+			if (_suspend_callback)
+			{
+				_suspend_callback();
+			}
 		}
 
 		void OnResumeCallback()
 		{
+			if (_resume_callback)
+			{
+				_resume_callback();
+			}
 		}
 
 		void OnConnectCallback()
 		{
+			if (_connect_callback)
+			{
+				_connect_callback();
+			}
 		}
 
 		void OnDisconnectCallback()
 		{
+			if (_disconnect_callback)
+			{
+				_disconnect_callback();
+			}
 		}
 
 		void OnDataOutStageCallback(uint8_t epnum)
@@ -87,6 +125,55 @@ namespace bsp
 		static PCD_HandleTypeDef &HalPcdHandle()
 		{
 			return hal_pcd_handle_context::_handle;
+		}
+
+		void SetSofCallback(std::function<void()> const callback)
+		{
+			base::interrupt::disable_interrupt(static_cast<int32_t>(IRQn_Type::OTG_HS_IRQn));
+			_sof_callback = callback;
+			base::interrupt::enable_interrupt(static_cast<int32_t>(IRQn_Type::OTG_HS_IRQn), 5);
+		}
+
+		void SetSetupStageCallback(std::function<void()> const callback)
+		{
+			base::interrupt::disable_interrupt(static_cast<int32_t>(IRQn_Type::OTG_HS_IRQn));
+			_setup_stage_callback = callback;
+			base::interrupt::enable_interrupt(static_cast<int32_t>(IRQn_Type::OTG_HS_IRQn), 5);
+		}
+
+		void SetResetCallback(std::function<void()> const callback)
+		{
+			base::interrupt::disable_interrupt(static_cast<int32_t>(IRQn_Type::OTG_HS_IRQn));
+			_reset_callback = callback;
+			base::interrupt::enable_interrupt(static_cast<int32_t>(IRQn_Type::OTG_HS_IRQn), 5);
+		}
+
+		void SetSuspendCallback(std::function<void()> const callback)
+		{
+			base::interrupt::disable_interrupt(static_cast<int32_t>(IRQn_Type::OTG_HS_IRQn));
+			_suspend_callback = callback;
+			base::interrupt::enable_interrupt(static_cast<int32_t>(IRQn_Type::OTG_HS_IRQn), 5);
+		}
+
+		void SetResumeCallback(std::function<void()> const callback)
+		{
+			base::interrupt::disable_interrupt(static_cast<int32_t>(IRQn_Type::OTG_HS_IRQn));
+			_resume_callback = callback;
+			base::interrupt::enable_interrupt(static_cast<int32_t>(IRQn_Type::OTG_HS_IRQn), 5);
+		}
+
+		void SetConnectCallback(std::function<void()> const callback)
+		{
+			base::interrupt::disable_interrupt(static_cast<int32_t>(IRQn_Type::OTG_HS_IRQn));
+			_connect_callback = callback;
+			base::interrupt::enable_interrupt(static_cast<int32_t>(IRQn_Type::OTG_HS_IRQn), 5);
+		}
+
+		void SetDisconnectCallback(std::function<void()> const callback)
+		{
+			base::interrupt::disable_interrupt(static_cast<int32_t>(IRQn_Type::OTG_HS_IRQn));
+			_disconnect_callback = callback;
+			base::interrupt::enable_interrupt(static_cast<int32_t>(IRQn_Type::OTG_HS_IRQn), 5);
 		}
 	};
 
