409c9f3f8ade56279a648ca275d95d653714dfff
hjc2000 <hjc.666@foxmail.com>
2025-09-25 09:34:07 +0800

    extern "C"

--- private_src/usb/isr.cpp
+++ private_src/usb/isr.cpp
@@ -1,14 +1,17 @@
 #include "hal.h" // IWYU pragma: keep
 
-extern PCD_HandleTypeDef hpcd_USB_OTG_FS;
-
-void OTG_HS_IRQHandler(void)
+extern "C"
 {
-	/* USER CODE BEGIN OTG_FS_IRQn 0 */
+	extern PCD_HandleTypeDef hpcd_USB_OTG_FS;
 
-	/* USER CODE END OTG_FS_IRQn 0 */
-	HAL_PCD_IRQHandler(&hpcd_USB_OTG_FS);
-	/* USER CODE BEGIN OTG_FS_IRQn 1 */
+	void OTG_HS_IRQHandler(void)
+	{
+		/* USER CODE BEGIN OTG_FS_IRQn 0 */
 
-	/* USER CODE END OTG_FS_IRQn 1 */
+		/* USER CODE END OTG_FS_IRQn 0 */
+		HAL_PCD_IRQHandler(&hpcd_USB_OTG_FS);
+		/* USER CODE BEGIN OTG_FS_IRQn 1 */
+
+		/* USER CODE END OTG_FS_IRQn 1 */
+	}
 }
