d84a09e1d2b7445ecbe6e206649230da8cce094d
hjc2000 <hjc.666@foxmail.com>
2025-09-25 09:48:46 +0800

    将底层初始化的工作转移了

--- private_src/usb/USB_DEVICE/Target/usbd_conf.cpp
+++ private_src/usb/USB_DEVICE/Target/usbd_conf.cpp
@@ -69,49 +69,6 @@ USBD_StatusTypeDef USBD_Get_USB_Status(HAL_StatusTypeDef hal_status);
 
 void HAL_PCD_MspInit(PCD_HandleTypeDef *pcdHandle)
 {
-	GPIO_InitTypeDef GPIO_InitStruct = {0};
-	RCC_PeriphCLKInitTypeDef PeriphClkInitStruct = {0};
-	if (pcdHandle->Instance == USB_OTG_HS)
-	{
-		/* USER CODE BEGIN USB_OTG_FS_MspInit 0 */
-
-		/* USER CODE END USB_OTG_FS_MspInit 0 */
-
-		/** Initializes the peripherals clock
-		 */
-		PeriphClkInitStruct.PeriphClockSelection = RCC_PERIPHCLK_USB;
-		PeriphClkInitStruct.UsbClockSelection = RCC_USBCLKSOURCE_PLL;
-		if (HAL_RCCEx_PeriphCLKConfig(&PeriphClkInitStruct) != HAL_OK)
-		{
-			throw std::runtime_error{CODE_POS_STR + "初始化失败。"};
-		}
-
-		/** Enable USB Voltage detector
-		 */
-		HAL_PWREx_EnableUSBVoltageDetector();
-
-		__HAL_RCC_GPIOA_CLK_ENABLE();
-		/**USB_OTG_FS GPIO Configuration
-		PA11     ------> USB_OTG_FS_DM
-		PA12     ------> USB_OTG_FS_DP
-		*/
-		GPIO_InitStruct.Pin = GPIO_PIN_11 | GPIO_PIN_12;
-		GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
-		GPIO_InitStruct.Pull = GPIO_NOPULL;
-		GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
-		GPIO_InitStruct.Alternate = GPIO_AF10_OTG1_FS;
-		HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
-
-		/* Peripheral clock enable */
-		__HAL_RCC_USB_OTG_HS_CLK_ENABLE();
-
-		/* Peripheral interrupt init */
-		HAL_NVIC_SetPriority(OTG_HS_IRQn, 0, 0);
-		HAL_NVIC_EnableIRQ(OTG_HS_IRQn);
-		/* USER CODE BEGIN USB_OTG_FS_MspInit 1 */
-
-		/* USER CODE END USB_OTG_FS_MspInit 1 */
-	}
 }
 
 void HAL_PCD_MspDeInit(PCD_HandleTypeDef *pcdHandle)
